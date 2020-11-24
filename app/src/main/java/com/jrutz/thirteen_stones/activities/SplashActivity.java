0e16a39e87bf8925c20b4d2855a8b5e5ab3f102e
yossi <yrotenstreich@gmail.com>
2020-11-24 18:11:50 -0500

    Created Splash Activity

---
+++ app/src/main/java/com/jrutz/thirteen_stones/activities/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.jrutz.thirteen_stones.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
